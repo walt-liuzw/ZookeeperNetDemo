2b71f45f81003c68702cbeebd5b6c69875b8487c
liuzhiwei <liuzhiwei@beisen.com>
2019-02-12 15:12:23 +0800

    InitialRepo

---
+++ ZookeeperDemo/ZookeeperDemo/Watcher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ZooKeeperNet;
+
+namespace ZookeeperDemo
+{
+    class Watcher : IWatcher
+    {
+        public void Process(WatchedEvent @event)
+        {
+            //if (@event.Type == EventType.NodeDataChanged)
+            //{
+                Console.WriteLine("已经触发了" + @event.Type + "事件！");
+            //}
+        }
+    }
+}
